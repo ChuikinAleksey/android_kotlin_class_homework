39ac48d1409fde647a8ca4509577bb5f52bc112f
AleksChuykin <chuikin.aleksey.7@gmail.com>
2025-12-18 12:45:50 +0300

    Update ClassHomework.kt

--- app/src/main/java/com/example/kotlinclasshomework/kotlinClass/ClassHomework.kt
+++ app/src/main/java/com/example/kotlinclasshomework/kotlinClass/ClassHomework.kt
@@ -8,6 +8,17 @@ package com.example.kotlinclasshomework.kotlinClass
  * После этого создай объект своего класса в методе main(), как мы это делали на уроке с другими классами и вызови у него методы goHome и haveLunch.
  * Посмотри, что будет выведено в терминал
  */
+class Profession: Employee () {
+    override fun haveLunch() {
+        println("You don't have lunch")
+    }
+    override fun goHome() {
+        println("You can go home early")
+    }
+}     
 fun main() {
+    val obj = Profession()
+    obj.haveLunch()
+    obj.goHome()
     // TODO здесь нужно создать объект своего класса и вызывать у него методы haveLunch() и goHome()
 }
